75037c72adb9138a81e7f4b8bf496f72daaf350b
MaitreyiJ <59170109+MaitreyiJ@users.noreply.github.com>
2020-08-28 02:53:01 -0400

    Update matAlgorithm.py
    -matAlgorithm.py-imports and loads the file"resultFile.mat" not included here (due to size issues) from uNIT code, to benchmark it with other algorithms

--- Benchmarking/matAlgorithm.py
+++ Benchmarking/matAlgorithm.py
@@ -21,24 +21,20 @@ from sklearn.cross_decomposition import PLSRegression
 from mpl_toolkits import mplot3d
 
 
-#so we are done with the pipeline, now we need to compare this algorithm to ground truth
-#We will be comparing the features of each of our waveforms(waveform) with the mean_cluster forms
-#for spike classification
-#time = np.linspace(0, wave_form.shape[1]/sampleRate, wave_form.shape[1])*1000
-#cluster_mean = wave_form[cluster==0, :].mean(axis=0)
+#HOW TO USE THE LOADMAT FUNCTION TO LOAD"RESULTFILE.MAT FROM UNIT CODE INTO SPIKEINTERFACE
 
 #Adding the code for comparision of spike sorting data with sorting extractor
 from scipy.io import loadmat
 x = loadmat('resultfile.mat')
-sortingPipeline = se.NumpySortingExtractor()
-labels=x['idx']
-times_reference=x['spike_idxs']
-sampleRate=x['Fs']
-sortingPipeline.set_times_labels(times=times_reference[0], labels=labels)
+sortingPipeline = se.NumpySortingExtractor() #initiating the sorting extractor object(refer spikeinterface tut on details aboyt sorting extractor)
+labels=x['idx'] #(Spike labels)
+times_reference=x['spike_idxs'] #(times where spikes are detected)
+sampleRate=x['Fs']  #(sampling frequency)
+sortingPipeline.set_times_labels(times=times_reference[0], labels=labels)   #(FEED THIS DATA FROM RESULT FILE TO SORTING EXTRACTOR)
 sortingPipeline.set_sampling_frequency(sampling_frequency=sampleRate)
 
 
-print('Unit ids = {}'.format(sortingPipeline.get_unit_ids()))
+print('Unit ids = {}'.format(sortingPipeline.get_unit_ids()))#(VERIFY DATA CORRECTLY STORED INTO SORTING EXTRACTOR)
 st = sortingPipeline.get_unit_spike_train(unit_id=1)
 print('Num. events for unit 1 = {}'.format(len(st)))
 st1 = sortingPipeline.get_unit_spike_train(unit_id=1)
